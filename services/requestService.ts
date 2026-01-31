7c7c4999abfc11e1364808b9ef6513d99a59b9af
John Doe <johndoe@example.com>
2026-01-31 16:03:40 +0700

    Fetch => Axios

--- services/requestService.ts
+++ services/requestService.ts
@@ -1,6 +1,9 @@
+import axios from "axios";
+
 class RequestService {
     private static instance: RequestService;
     private url: string = process.env.VITE_API_URL;
+    private headers = { 'Content-Type': 'application/json' };
 
     public static getInstance(): RequestService {
         if (!RequestService.instance) {
@@ -10,19 +13,23 @@ class RequestService {
     }
 
     public get(endpoint: string, body: any = {}, options?: { method?: string }): Promise<any> {
-        return fetch(`${this.url}/${endpoint}`, {
-            method: options?.method || 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: options?.method === 'GET' ? undefined : JSON.stringify(body)
+        const method = options?.method || 'POST';
+        const config = {
+            data: body,
+            url: `${this.url}/${endpoint}`,
+        };
+        return axios({
+            headers: this.headers,
+            method,
+            ...config,
         })
-        .then(response => {
-            if (!response.ok) {
+            .then(response => response.data)
+            .catch(error => {
+                if (axios.isAxiosError(error) && error.response) {
+                    throw new Error(error.response.data?.message || 'Failed to fetch books');
+                }
                 throw new Error('Failed to fetch books');
-            }
-            return response.json();
-        });
+            });
     }
 }
 
